152650416f5264e11fbfa5489dd07a7fa90b4675
Paulo Campos <paulorcv@gmail.com>
2015-08-04 15:25:26 -0300

    Pacote com.ninjagaiden deletado da raiz

--- com/ninjagaiden/Main.java
+++
@@ -1,8 +0,0 @@
-package com.ninjagaiden;
-
-public class Main {
-
-    public static void main(String[] args) {
-    	System.out.println("Hello Everybody! This is the begin of nothing or maybe a huge thing :)");    	
-    }
-}
